67c3487d3622e60badb160ab7e6c371820169c6e
EmilioSegura98 <112990749+EmilioSegura98@users.noreply.github.com>
2023-04-01 17:43:32 -0600

    Add files via upload

---
+++ Employee_Database_challenge.sql
@@ -0,0 +1,41 @@
+-- The Number of Retiring Employees by Title
+SELECT em.emp_no, 
+        em.first_name, 
+        em.last_name,
+	    ti.title, 
+        ti.from_date, 
+        ti.to_date
+	INTO retiring_employees_by_title
+	FROM employees AS em
+	LEFT JOIN titles as ti
+	ON (em.emp_no = ti.emp_no)
+	WHERE (em.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+	ORDER BY em.emp_no;
+
+-- Unique Titles Table
+SELECT DISTINCT ON (emp_no) emp_no, 
+	first_name, 
+    last_name, 
+    title
+INTO unique_titles
+FROM retiring_employees_by_title
+ORDER BY emp_no ASC, to_date DESC;
+
+SELECT COUNT(title), title 
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY COUNT(title) DESC;
+
+
+-- The Employees Eligible for the Mentorship Program
+SELECT DISTINCT ON (em.emp_no) em.emp_no, em.first_name, em.last_name, em.birth_date, de.from_date, de.to_date, ti.title
+INTO mentorship_elegible 
+FROM employees AS em
+LEFT JOIN dept_emp AS de
+ON em.emp_no = de.emp_no
+LEFT JOIN titles AS ti
+ON em.emp_no = ti.emp_no
+WHERE (em.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+AND (de.to_date = '9999-01-01')
+ORDER BY em.emp_no;
